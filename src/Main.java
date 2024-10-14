7074d15c49a804e44e05c694b7524d61cca0b0bc
Thang0226 <thang.nd0226@gmail.com>
2024-10-15 06:02:21 +0700

    update book names

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) {
 		Book[] books = new Book[6];
-		books[0] = new Book("123", "Abc", 456, "Thang");
-		books[1] = new Book("456", "Cde", 789, "Tri");
-		books[2] = new Book("789", "Ghi", 1234, "Dung");
-		books[3] = new Book("1234", "Jkl", 1500, "Vinh");
-		books[4] = new Book("1500", "Mno", 2345, "Nhat");
-		books[5] = new Book("2345", "Pqr", 6789, "Loc");
+		books[0] = new Book("0123", "5 phuong trinh lam thay doi the gioi", 456, "Thang");
+		books[1] = new Book("0456", "Sinh vao ngay xanh", 789, "Tri");
+		books[2] = new Book("0789", "Ban mai tre", 1234, "Dung");
+		books[3] = new Book("1234", "Cuoc chien lo den", 1500, "Vinh");
+		books[4] = new Book("1500", "Mat ma", 2345, "Nhat");
+		books[5] = new Book("2345", "Luoc su thoi gian", 6789, "Loc");
 
 		float total = 0;
 		for (Book book : books) {
@@ -17,10 +17,9 @@ public class Main {
 		System.out.println("Total book price: " + total);
 
 		System.out.println("Books that have price over 1000: ");
-		System.out.print("\t");
 		for (Book book : books) {
 			if (book.getPrice() > 1000) {
-				System.out.print(book.getBookName() + "; ");
+				System.out.println("\t" + book.getBookName());
 			}
 		}
 	}
